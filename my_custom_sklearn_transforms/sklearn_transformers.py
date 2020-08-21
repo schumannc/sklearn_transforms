47ede65a62de8b87541ef4531eae4143c6b23581
henrique <henriquescosta@gmail.com>
2020-08-21 17:36:24 -0300

    add custom transformers

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,29 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+
+class AggFeatures(BaseEstimator, TransformerMixin):
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        df = X.copy()
+        
+        notas_cols = ['NOTA_DE', 'NOTA_EM', 'NOTA_MF', 'NOTA_GO']
+        rep_cols = ['REPROVACOES_DE', 'REPROVACOES_EM', 'REPROVACOES_MF', 'REPROVACOES_GO']
+        
+        notas = df[notas_cols].mean(axis=1)
+        total_rep = df[rep_cols].sum(axis=1)
+        
+        notas = (notas / 10).clip(0, 1)
+        reprovacoes = (total_rep / (3 * 4)).clip(0, 1)
+        h_aula = (df["H_AULA_PRES"] / 25).clip(0, 1)
+        faltas = (df["FALTAS"] / 8).clip(0, 1)
+        tarefas = (df["TAREFAS_ONLINE"] / 7).clip(0, 1)
+        
+        df['SCORE_BOM'] = (notas + h_aula + tarefas) / 3
+        df['SCORE_RUIM'] = (reprovacoes + faltas) / 2
+
+        return df
